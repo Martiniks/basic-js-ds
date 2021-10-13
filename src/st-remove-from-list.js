f3ceb1808097acec76077cc944ee203fb6fd45c7
Oleg Sokolovsky <Martiniks.so@gmail.com>
2021-10-13 23:21:52 +0300

    feat:  add function removeKFromList

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -1,4 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
+const {ListNode} = require('../extensions/index.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -19,7 +20,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+    let arr = [];
+    let value = l.value;
+    let next = l.next;
+    while (next) {
+        arr.push(value);
+        value = next.value;
+        next = next.next;
+    }
+    arr.push(value);
+
+    let newArr = Array.from(arr).filter(elem => elem !== k);
+    return convertArrayToList(newArr);
+
+
+    function convertArrayToList(arr) {
+        return arr.reverse().reduce((acc, cur) => {
+            if (acc) {
+                const node = new ListNode(cur);
+                node.next = acc;
+                return node;
+            }
+
+            return new ListNode(cur);
+        }, null);
+    }
+
 }
