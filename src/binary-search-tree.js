f715259e7ef7a73087a0ec036d970e1affdf19c3
Oleg Sokolovsky <Martiniks.so@gmail.com>
2021-10-15 17:00:49 +0300

    feat: add class binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,14 +8,44 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 module.exports = class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.data = null;
+    this.left = null;
+    this.right = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    if (this.data==null) {
+      return null
+    }
+    return {data: this.data, left: this.left, right: this.right}
+  }
+
+  add( data ) {
+    if (!this.data) {
+      this.data = data;
+      return
+    }
+    // let minData=this.data;
+
+    if (this.data < data) {
+      let minData = this.data;
+      this.data = data;
+      data = minData;
+    }
+
+    if (!this.left) {
+      this.left = new Node(data);
+      return;
+    }
+
+    if (!this.right) {
+      this.right = new Node(data);
+      return;
+    }
+
+
+
   }
 
   has(/* data */) {
